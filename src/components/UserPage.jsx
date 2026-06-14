361c1dc40e1b91dfd35dd7fd166ec5c0ba1aee96
udaya <udayabca2021@gmail.com>
2026-06-15 00:42:10 +0530

    ner

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -24,7 +24,19 @@ const UserPage = () => {
         }
 
         const pc = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' },
+                { 
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
+                { 
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                }
+            ]
         });
         peerConnection.current = pc;
 
